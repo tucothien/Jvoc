bbe11e5fa0b13961ba9292ddf6c2ed408c5082a7
DESKTOP-0R12G3H\ntt <tucothien@gmail.com>
2024-11-30 11:11:32 +0900

    Remove kanji option

--- js/full_kanji.js
+++ js/full_kanji.js
@@ -43,74 +43,74 @@ data_kanji = [
         "onyomi": "く",
         "kunyomi": "にが、くる",
         "info": "古 (cổ) 艹 (thảo)",
-        "examples": ["苦い (にがい): đắng", ]
+        "examples": ["苦い (にがい): đắng", "苦しい (くるしい): cực khổ", "苦しむ (くるしむ): (V) cực khổ"]
       },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
+        "vietnamese": "thống",
+        "kanji": "痛",
+        "onyomi": "つう",
+        "kunyomi": "いた",
+        "info": "疒 (nạch) 甬 (dũng) 通 (thông)",
+        "examples": ["痛い (いたい): đau", "痛み (いたみ): cơn đau"]
+      },{
+        "vietnamese": "sỉ",
+        "kanji": "恥",
+        "onyomi": "ち",
+        "kunyomi": "は、はじ",
+        "info": "耳 (nhĩ) 心 (tâm)",
+        "examples": ["恥ずかしい (はずかしい): xấu hổ", "恥じ (はじ): sự xấu hổ"]
+      },{
+        "vietnamese": "phối",
+        "kanji": "配",
+        "onyomi": "ぱい、はい",
+        "kunyomi": "くば",
+        "info": "酉 (dậu) 己 (kỉ)",
+        "examples": ["心配する (しんぱいする): lo lắng", "配る (くばる): phân phát"]
+      },{
+        "vietnamese": "khốn",
+        "kanji": "困",
+        "onyomi": "こん",
+        "kunyomi": "こま",
+        "info": "",
+        "examples": ["困る (こまる): khó khăn"]
+      },{
+        "vietnamese": "tân",
+        "kanji": "辛",
+        "onyomi": "しん",
+        "kunyomi": "から",
+        "info": "",
+        "examples": ["辛い (からい): cay", "塩辛い (しおからい): mặn"]
+      },{
+        "vietnamese": "miên",
+        "kanji": "眠",
+        "onyomi": "みん",
+        "kunyomi": "ねむ",
+        "info": "",
+        "examples": [""]
+      },{
+        "vietnamese": "tàn",
+        "kanji": "殘",
+        "onyomi": "ざん",
+        "kunyomi": "のこ",
+        "info": "",
+        "examples": [""]
+      },{
+        "vietnamese": "niệm",
+        "kanji": "念",
+        "onyomi": "ねん",
         "kunyomi": "",
         "info": "",
         "examples": [""]
       },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
+        "vietnamese": "cảm",
+        "kanji": "感",
+        "onyomi": "かん",
         "kunyomi": "",
         "info": "",
         "examples": [""]
       },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
-        "kunyomi": "",
-        "info": "",
-        "examples": [""]
-      },{
-        "vietnamese": "",
-        "kanji": "",
-        "onyomi": "",
+        "vietnamese": "tình",
+        "kanji": "情",
+        "onyomi": "じょう",
         "kunyomi": "",
         "info": "",
         "examples": [""]
